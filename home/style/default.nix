c6ed9f0c5a7e0c980e079609ef703eae7edde34d
Jurn Wubben <jurnwubben@gmail.com>
2025-02-17 21:04:03 +0100

    Removed legacy code.

--- home/style/default.nix
+++ home/style/default.nix
@@ -1,6 +1,27 @@
 {
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
-    #./pointer.nix
-    ./stylix.nix
+    inputs.stylix.homeManagerModules.stylix
   ];
+
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 21;
+    };
+    fonts.monospace = {
+      name = "DejaVuSansMNerdFontMono-Regular";
+      package = pkgs.nerd-fonts.dejavu-sans-mono;
+    };
+
+    #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
+    image = ./background.png;
+  };
 }
