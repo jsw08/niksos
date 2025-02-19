0a9889ec51cb03ac1e2b4c41212ab9b731220d9f
Jurn Wubben <jurnwubben@gmail.com>
2025-02-19 19:20:03 +0100

    Removed legacy files. Switch to different screenshot editor. Made fingerprintreader work!

--- home/style/default.nix
+++ home/style/default.nix
@@ -10,18 +10,26 @@
 
   stylix = {
     enable = true;
+
+    image = ./background.png;
     polarity = "dark";
+
+    fonts.monospace = {
+      name = "DejaVuSansMNerdFontMono-Regular";
+      package = pkgs.nerd-fonts.dejavu-sans-mono;
+    };
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
       size = 21;
     };
-    fonts.monospace = {
-      name = "DejaVuSansMNerdFontMono-Regular";
-      package = pkgs.nerd-fonts.dejavu-sans-mono;
+    iconTheme = {
+      enable = true;
+      package = pkgs.tela-circle-icon-theme; #FIXME: Swap out for normal tela icons once https://github.com/NixOS/nixpkgs/issues/381521 is upstream.
+      dark = "Tela-circle-dark";
+      light = "Tela-circle-dark";
     };
 
     #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
-    image = ./background.png;
   };
 }
--- home/wayland/hyprlock.nix
+++ home/wayland/hyprlock.nix
@@ -1,9 +1,6 @@
-{
-  config,
-  inputs,
-  pkgs,
-  ...
-}: {
+{config, ...}: let
+  inherit (config.lib.stylix.colors) base05;
+in {
   programs.hyprlock = {
     enable = true;
 
@@ -14,49 +11,16 @@
         hide_cursor = false;
         no_fade_in = true;
       };
-
-      # background = [
-      #   {
-      #     monitor = "";
-      #     path = config.theme.wallpaper;
-      #   }
-      # ];
-
-      # input-field = [
-      #   {
-      #     size = "300, 50";
-      #     valign = "bottom";
-      #     position = "0%, 10%";
-      #
-      #     outline_thickness = 1;
-      #
-      #     # font_color = "rgb(b6c4ff)";
-      #     # outer_color = "rgba(180, 180, 180, 0.5)";
-      #     # inner_color = "rgba(200, 200, 200, 0.1)";
-      #     # check_color = "rgba(247, 193, 19, 0.5)";
-      #     # fail_color = "rgba(255, 106, 134, 0.5)";
-      #
-      #     fade_on_empty = false;
-      #     placeholder_text = "Enter Password";
-      #
-      #     dots_spacing = 0.2;
-      #     dots_center = true;
-      #     dots_fade_time = 100;
-      #
-      #     # shadow_color = "rgba(0, 0, 0, 0.1)";
-      #     shadow_size = 7;
-      #     shadow_passes = 2;
-      #   }
-      # ];
+      auth."fingerprint:enabled" = true;
 
       label = [
         {
           monitor = "";
           text = "$TIME";
           font_size = 150;
-          color = "rgb(b6c4ff)";
+          color = "rgb(${base05})";
 
-          position = "0%, 30%";
+          position = "0%, 33%";
 
           valign = "center";
           halign = "center";
@@ -70,7 +34,7 @@
           monitor = "";
           text = "cmd[update:3600000] date +'%a %b %d'";
           font_size = 20;
-          color = "rgb(b6c4ff)";
+          color = "rgb(${base05})";
 
           position = "0%, 40%";
 
