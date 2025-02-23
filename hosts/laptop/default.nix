a752457f13d7099485892cc74d9b3f050432968d
Jurn Wubben <jurnwubben@gmail.com>
2025-02-23 01:12:16 +0100

    Laptop: hyprland monitor config; firefox; force searchengine; neovim config good enough™

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -4,8 +4,7 @@
     ./virt.nix
   ];
 
-  virtualisation.docker.enable = true;
-
+  home-manager.users.jsw.wayland.windowManager.hyprland.settings.monitor = ["eDP-1,2880x1920@120,0x0,1.5,vrr,1"];
   services = {
     fprintd.enable = true;
     udev.extraRules = ''
