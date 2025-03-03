8d59cbf6449f6c9df77c601b7b6aaaf6958d975f
Jurn Wubben <jurnwubben@gmail.com>
2025-03-03 18:14:56 +0100

    small changes

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -1,6 +1,20 @@
-{pkgs, ...}: {
+# {
+#   lib,
+#   config,
+#   ...
+# }: {
+#   options.niksos.bluetooth = lib.mkEnableOption;
+#
+#   config = lib.mkIf config.niksos.bluetooth {
+#     hardware.bluetooth = {
+#       enable = true;
+#       input.General.ClassicBondedOnly = false;
+#     };
+#   };
+# }
+{
   hardware.bluetooth = {
     enable = true;
+    input.General.ClassicBondedOnly = false;
   };
-  environment.systemPackages = [pkgs.bluetui];
 }
