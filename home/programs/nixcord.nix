c48b41c250209f5cece84fc62484eb5c5887989f
Jurn Wubben <jurnwubben@gmail.com>
2025-03-10 09:28:32 +0100

    Nixcord stuff; Updated games; Added python lsp

---
+++ home/programs/nixcord.nix
@@ -0,0 +1,63 @@
+{inputs, ...}: {
+  imports = [inputs.nixcord.homeManagerModules.nixcord];
+
+  programs.nixcord = {
+    enable = true;
+
+    discord.enable = false;
+    vesktop.enable = true;
+    vesktopConfig = {
+      tray = false;
+      minimizeToTray = false;
+      enableSplashScreen = false;
+    };
+
+    config = {
+      frameless = true;
+      transparent = true;
+
+      plugins = let
+        enabledPlugins = [
+          # Plugins without config
+          "anonymiseFileNames"
+          "betterRoleDot"
+          "betterSettings"
+          "biggerStreamPreview"
+          "callTimer"
+          "clearURLs"
+          "copyFileContents"
+          "emoteCloner"
+          "fakeNitro"
+          "fixYoutubeEmbeds"
+          "friendsSince"
+          "fullSearchContext"
+          "memberCount"
+          "mentionAvatars"
+          "noUnblockToJump"
+          "openInApp"
+          "petpet"
+          "previewMessage"
+          "quickReply"
+          "relationshipNotifier"
+          "secretRingToneEnabler"
+          "shikiCodeblocks"
+          "showHiddenChannels"
+          "showHiddenThings"
+          "showMeYourName"
+          "unindent"
+          "userVoiceShow"
+          "youtubeAdblock"
+          "webScreenShareFixes"
+        ];
+      in
+        (builtins.listToAttrs (map (x: {
+            name = x;
+            value = {enable = true;};
+          })
+          enabledPlugins))
+        // {
+          # Plugins with config
+        };
+    };
+  };
+}
