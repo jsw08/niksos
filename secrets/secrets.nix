813a35301866d828b50b254aacf922aa0cb43a54
Jurn Wubben <jurnwubben@gmail.com>
2025-03-28 00:15:01 +0100

    Added lapserv to agenix.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHB3qkRCskSMiAs2kLTsG+ruESK4h1pP1FHm+rVnKWx4";
+  lapserv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOx/ic0gwhQGpUnlQbpnW5TisPXV9TQgNz6JrQ/vjhaL";
 
-  systems = [laptop];
+  systems = [laptop lapserv];
 in {
   "transfer-sh.age".publicKeys = systems;
   "password.age".publicKeys = systems;
