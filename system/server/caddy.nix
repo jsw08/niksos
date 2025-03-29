cdfa2a3a0389fa5a256b83029fc2c67842c4ff90
Jurn Wubben <jurnwubben@gmail.com>
2025-03-29 11:17:20 +0100

    Moved caddy virtual host part to service file.

--- system/server/caddy.nix
+++ system/server/caddy.nix
@@ -3,13 +3,6 @@
     enable = config.niksos.server;
     email = "jurnwubben@gmail.com";
     enableReload = false;
-
-    virtualHosts."share.jsw.tf" = {
-      serverAliases = ["www.share.jsw.tf"];
-      extraConfig = ''
-        reverse_proxy :9000
-      '';
-    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
