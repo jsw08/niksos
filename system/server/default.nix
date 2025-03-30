104a592b1118586c00d5712d5244bf982830172e
Jurn Wubben <jurnwubben@gmail.com>
2025-03-30 11:25:45 +0000

    Seafile minimal config working, now modularise and configure.

--- system/server/default.nix
+++ system/server/default.nix
@@ -1,4 +1,4 @@
 {lib, ...}: {
-  imports = [./caddy.nix ./transfer-sh.nix];
+  imports = [./caddy.nix ./transfer-sh.nix ./seafile.nix];
   options.niksos.server = lib.mkEnableOption "server servcies (such as caddy)."; #TODO: per service option.
 }
