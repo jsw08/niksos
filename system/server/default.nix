215a8d138ad9f1128ccf5621da0ba5658f9cc5f9
Jurn Wubben <jurnwubben@gmail.com>
2025-04-02 08:21:55 +0200

    Working on dc bot for server

--- system/server/default.nix
+++ system/server/default.nix
@@ -1,4 +1,4 @@
 {lib, ...}: {
-  imports = [./caddy.nix ./transfer-sh.nix ./seafile.nix];
+  imports = [./caddy.nix ./transfer-sh.nix ./seafile.nix ./bot.nix];
   options.niksos.server = lib.mkEnableOption "server servcies (such as caddy)."; #TODO: per service option.
 }
