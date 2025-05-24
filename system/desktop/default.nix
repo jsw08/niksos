baaa3237a2ebf4d0db8aaab120d5c4f6fcdcaaf0
Jurn Wubben <jurnwubben@gmail.com>
2025-05-24 15:08:55 +0200

    Added assertions for desktop options

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -1,5 +1,12 @@
-{lib, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   mkEO = lib.mkEnableOption;
+  cfg = config.niksos.desktop;
+
+  inherit (lib) mkIf optional;
 in {
   imports = [
     ./greetd.nix
@@ -15,4 +22,12 @@ in {
     hyprland = mkEO "enable hyprland related programs.";
     kde = mkEO "enable kde specialisation.";
   };
+
+  config.assertions = mkIf (cfg.hyprland
+    || cfg.kde) [
+    {
+      assertion = cfg.enable;
+      message = "You need to enable desktop for the hyprland/kde module to work";
+    }
+  ];
 }
