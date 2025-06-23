3cd43ca114fdad11112316d6f4424bcfa48bf93d
Jurn Wubben <jurnwubben@gmail.com>
2025-06-23 20:26:51 +0200

    Added forgejo

---
+++ system/server/forgejo.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  DOMAIN = "git.jsw.tf";
+in {
+  config =
+    lib.mkIf config.niksos.server
+    {
+      services.caddy.virtualHosts.${DOMAIN}.extraConfig = ''
+        request_body {
+            max_size 512M
+        }
+        reverse_proxy localhost:9004
+      '';
+
+      services.forgejo = {
+        enable = true;
+        database.type = "postgres";
+        lfs.enable = true;
+
+        settings = {
+          server = {
+            inherit DOMAIN;
+            ROOT_URL = "https://${DOMAIN}/";
+            HTTP_PORT = 9004;
+          };
+          service.DISABLE_REGISTRATION = false;
+          actions = {
+            ENABLED = true;
+            DEFAULT_ACTIONS_URL = "github";
+          };
+        };
+      };
+    };
+}
