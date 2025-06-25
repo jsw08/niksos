5ba57fc45ec799885fb0ebb5d2425f38b9f4b2d2
Jurn Wubben <jurnwubben@gmail.com>
2025-06-25 18:16:16 +0200

    Added temporary service

---
+++ system/server/temp.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  config = lib.mkIf config.niksos.server {
+    # NOTE: allows me to spin up temporarily services.
+    services.caddy.virtualHosts."temp.jsw.tf".extraConfig = ''
+      reverse_proxy :8000
+    '';
+  };
+}
