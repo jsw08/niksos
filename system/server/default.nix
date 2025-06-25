418024dcc3f74f1648a537f4fe81037e5435f781
Jurn Wubben <jurnwubben@gmail.com>
2025-06-25 18:20:57 +0200

    Activated temporary service

--- system/server/default.nix
+++ system/server/default.nix
@@ -1,4 +1,4 @@
 {lib, ...}: {
-  imports = [./caddy.nix ./index ./seafile.nix ./bot.nix ./immich.nix ./matrix.nix ./mail.nix ./forgejo.nix];
+  imports = [./caddy.nix ./index ./seafile.nix ./bot.nix ./immich.nix ./matrix.nix ./mail.nix ./forgejo.nix ./temp.nix];
   options.niksos.server = lib.mkEnableOption "server servcies (such as caddy)."; #TODO: per service option.
 }
