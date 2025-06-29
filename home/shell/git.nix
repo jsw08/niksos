f5d470e6984c65a3ebdb47a469d089002818b594
Jurn Wubben <jurnwubben@gmail.com>
2025-06-29 23:19:12 +0200

    Enabled lfs for desktop

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -5,6 +5,7 @@
       userEmail = "jurnwubben@gmail.com";
       userName = "Jurn Wubben";
       extraConfig.push.autoSetupRemote = true;
+      lfs.enable = true;
     };
     git-credential-oauth.enable = true;
   };
