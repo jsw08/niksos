0406d043bdb081258e951e1b1b12e67b08f9c35e
Jurn Wubben <jurnwubben@gmail.com>
2025-07-20 13:16:41 +0200

    Rekeyed and changed secret keys.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,22 @@
 let
-  jswLaptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHB3qkRCskSMiAs2kLTsG+ruESK4h1pP1FHm+rVnKWx4";
-  jswLapserv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOx/ic0gwhQGpUnlQbpnW5TisPXV9TQgNz6JrQ/vjhaL";
+  users = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHB3qkRCskSMiAs2kLTsG+ruESK4h1pP1FHm+rVnKWx4" # jsw@laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOx/ic0gwhQGpUnlQbpnW5TisPXV9TQgNz6JrQ/vjhaL" # jsw@lapserv
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/WnwVZAUEC/mi7OxhGRXIMASHVj9t8Jbo06xa+NsjG" # jsw@desktop
+  ];
 
-  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZebJtRg3+dZYDGOw7dyjHL69i4MTFdoQEeHlW/AsDu";
-  lapserv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICyy+Tvkelrghc3Vftq8aTnUckec9BV8Ba8XAKCDc+Uw ";
-  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBBwuisiDr/WiZ4Q/HUIiPORiC8Ik47+qRvB0SL3enf";
+  devices = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZebJtRg3+dZYDGOw7dyjHL69i4MTFdoQEeHlW/AsDu" # laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICyy+Tvkelrghc3Vftq8aTnUckec9BV8Ba8XAKCDc+Uw " # lapserv
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBBwuisiDr/WiZ4Q/HUIiPORiC8Ik47+qRvB0SL3enf" # desktop
+  ];
 
-  systems = [laptop lapserv desktop jswLaptop jswLapserv];
+  keys = users ++ devices;
 in {
-  "transfer-sh.age".publicKeys = systems;
-  "password.age".publicKeys = systems;
-  "dcbot.age".publicKeys = systems;
-  "matrix-registration.age".publicKeys = systems;
-  "cloudflare-acme.age".publicKeys = systems;
-  "mail-admin.age".publicKeys = systems;
+  "transfer-sh.age".publicKeys = keys;
+  "password.age".publicKeys = keys;
+  "dcbot.age".publicKeys = keys;
+  "matrix-registration.age".publicKeys = keys;
+  "cloudflare-acme.age".publicKeys = keys;
+  "mail-admin.age".publicKeys = keys;
 }
